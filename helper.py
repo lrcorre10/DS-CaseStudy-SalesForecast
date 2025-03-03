71b5f34c0b8205df1d0b25d24b613ba44f2904df
lucas correa <lucas.correa@tradingcomdados.com>
2025-03-03 00:04:42 -0300

    add development

--- helper.py
+++ helper.py
@@ -70,7 +70,7 @@ def optuna_process(X_train, X_test, y_train, y_test):
         return rmse
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=10)
 
     print('Best trial:')
     trial = study.best_trial
